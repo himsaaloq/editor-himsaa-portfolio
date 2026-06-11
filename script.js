3658dc8b849b0f9f7177fc536ab4b0ad6de9f877
himsaaloq <himeshloq@gmail.com>
2026-06-11 13:56:35 +0530

    Hero section responsive fixes

--- script.js
+++ script.js
@@ -200,3 +200,51 @@ console.log(error);
 });
 });
 
+/* SCROLL PROGRESS BAR */
+
+window.addEventListener("scroll",()=>{
+
+let scrollTop =
+document.documentElement.scrollTop;
+
+let scrollHeight =
+document.documentElement.scrollHeight -
+document.documentElement.clientHeight;
+
+let progress =
+(scrollTop/scrollHeight)*100;
+
+document.getElementById("progress-bar")
+.style.width = progress + "%";
+
+});
+
+/* BACK TO TOP */
+
+const topBtn =
+document.getElementById("topBtn");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY > 300){
+
+topBtn.style.display="block";
+
+}else{
+
+topBtn.style.display="none";
+
+}
+
+});
+
+topBtn.addEventListener("click",()=>{
+
+window.scrollTo({
+
+top:0,
+behavior:"smooth"
+
+});
+
+});
